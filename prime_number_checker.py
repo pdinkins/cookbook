a153456886391dfd4e4f0b8d76ecdaed28d9e4b7
ParkerDinkins <parkerdinkins@gmail.com>
2018-02-01 12:35:27 -0500

    d
                                                                                  d

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -3,49 +3,62 @@
 
 
 # define a function
-def print_factors(x):
-    # This function takes a number and prints the factors
-
-    print("The factors of", x, "are:")
-    for i in range(1, x + 1):
-        if x % i == 0:
-            print(i)
-            # print('x=', x)
-    print(x, 'is not a prime number')
 
 
-def prime_checker():
-    try:
-        #a = int in range(0,5000)
-        a = int(input("Enter number between 0 and 5000: ")) #in range(0, 5000)
 
-        if a is False:
-            prime_checker()
-            #print('****ERROR****')
-            #print('Enter a number between 0 and 5000 please\n')
-            #prime_checker()
+class num_checkers():
+    def print_factors(x):
+        # This function takes a number and prints the factors
+        print("The factors of", x, "are:")
+        for i in range(1, x + 1):
+            if x % i == 0:
+                print(i)
+                # print('x=', x)
+        print(x, 'is not a prime number')
 
-        k = 0
-        for i in range(2, a // 3):
-            if a % i == 0:
-                k = k + 1
-
-        if k <= 0:
-            print('The factors of your number are:')
-            print('1')
-            print(a, 'is a PRIME number')
-
+        
+        
+    def num_input():
+        number = int(input('> '))
+        if number <= 5000:
+            number = num_input.cn
+            return num_input.cn
         else:
-            print_factors(x=a)
+            number = nocheck
+            
+    def prime_checker():
+        try:
+            #a = int in range(0,5000)
+            #a = int(input("Enter number between 0 and 5000: ")) #in range(0, 5000)
 
-    except ValueError:
-        print('****Invalid*Input****\nPlease enter a number\n')
+            #if a is False:
+            #   prime_checker()
+                #print('****ERROR****')
+                #print('Enter a number between 0 and 5000 please\n')
+                #prime_checker()
+            a = num_input.cn
+            k = 0
+            for i in range(2, a // 3):
+                if a % i == 0:
+                    k = k + 1
+
+            if k <= 0:
+                print('The factors of your number are:')
+                print('1')
+                print(a, 'is a PRIME number')
+
+            else:
+                print_factors(x=a)
+
+        except ValueError:
+            print('****Invalid*Input****\nPlease enter a number\n')
 
 
 run = True
 run1 = True
 
 while run:
+    num_input()
     prime_checker()
     break
 
